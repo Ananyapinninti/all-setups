df04323d55ef4ce8e38cb42b8e3a638ccd07aeeb
Ananyapinninti <ananyapinninti@gmail.com>
2025-08-21 15:02:56 +0530

    latest Update for ami-6.12 jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -14,3 +14,15 @@ yum install jenkins -y
 systemctl start jenkins.service
 systemctl enable jenkins.service
 systemctl status jenkins.service
+
+Latest update for AMI-6.12
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install git -y
+sudo yum install java-17-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
