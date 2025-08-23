e64d36da4e2a2ecd7ec1e188fa178f5d26ccde01
Ananyapinninti <ananyapinninti@gmail.com>
2025-08-23 23:46:03 +0530

    Update ArgoCD.sh

--- ArgoCD.sh
+++ ArgoCD.sh
@@ -5,7 +5,7 @@ kubectl get all -n argocd
 
 To expose ARGOCD SERVER :
 kubectl patch svc argocd-server -n argocd -p '{"spec":{"type":"LoadBalancer"}}'
-yum install jg -y
+yum install jq -y
 export ARGOCD_SERVER='kubectl get svc argocd-server -n argocd -o json | jq --raw-output '.status.loadBalancer.ingress[0].hostname''
 echo $ARGOCD_SERVER
 kubectl get svc argocd-server -n argocd -o json | jq --raw-output .status.loadBalancer.ingress[0].hostname
